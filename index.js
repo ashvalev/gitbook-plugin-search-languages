4dace27453a1a6f2009f7d59aaf58b06520a7e64
ashvalev <ashvalev@gmail.com>
2018-02-07 14:11:41 +0100

    Test commit #4

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ function getSearchIndex(context) {
         // Create search index
         var ignoreSpecialCharacters = context.config.get('pluginsConfig.searchLanguages.ignoreSpecialCharacters')
                                       || context.config.get('searchLanguages.ignoreSpecialCharacters');
-        // var lang = context.config.get('pluginsConfig.searchLanguages.lang');
-        var lang = 'en';
+        var lang = context.config.get('pluginsConfig.searchLanguages.lang');
         if (lang) {
             require('lunr-languages/lunr.stemmer.support.js')(lunr);
             if (lang === 'jp') {
